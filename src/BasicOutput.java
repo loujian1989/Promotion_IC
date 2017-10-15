5e4e5b5f25a8a777ee7fba134d572aef7e5f9ad0
loujian1989 <loujian1989@gmail.com>
2017-10-15 14:38:25 -0500

    add source code

---
+++ src/BasicOutput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Created by loujian on 9/1/17.
+ */
+public class BasicOutput {
+
+    public static void main(String[] args)throws Exception
+    {
+        int sum=0, number;
+        for(number=1; number<=4; number++)
+            sum= sum+number;
+        System.out.println(sum);
+    }
+
+}
